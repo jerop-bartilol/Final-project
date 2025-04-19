1ad8fd9ac052d91fdb981cbf008abb8aba3741ee
Jerop-bartilol <jeropbartilo@gmail.com>
2025-04-19 21:19:34 +0300

    project

--- js/index.js
+++ js/index.js
@@ -4,3 +4,29 @@ document.querySelectorAll(".faq-question").forEach(item => {
         parent.classList.toggle( 'active' );
     });
 });
+
+/* Contact form*/
+const Name = document.getElementById("name");
+const form = document.getElementById("form");
+const number = document.getElementById("Phonenumber");
+const errorElement = document.getElementById("error");
+
+form.addEventListener("submit" ,(e) =>{
+    let message =[];
+    if(Name.value === ""|| Name.value == null){
+        messages.push("Name is required");
+    }
+    if(number.value.length <= 6){
+        messages.push("Phone Number must be longer than 6 characters");
+    }
+    if(number.value.length >= 10){
+        message.push("Phone Number must not be longer than 10 characters");
+    }
+    if(messages.length > 0){
+        e.preventDefault()
+        errorElement.innerText = messages.join(" , ");
+    }
+});
+form.addEventListener("submit" , function(event){
+    event.preventDefault();
+});
